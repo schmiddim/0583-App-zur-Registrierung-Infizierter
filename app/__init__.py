dfb9d95b31c5cd2d11107d1b6f06192fa9858c64
Jakob Rohrhirsch <jakob@rohrhirsch-consulting.de>
2020-03-21 17:34:08 +0100

    SQLAlchemy session db in configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,30 @@
 from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import sessionmaker
+
+
+def db(request):
+    maker = request.registry.dbmaker
+    session = maker()
+
+    def cleanup(request):
+        if request.exception is not None:
+            session.rollback()
+        else:
+            session.commit()
+        session.close()
+    request.add_finished_callback(cleanup)
+
+    return session
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     with Configurator(settings=settings) as config:
+        engine = engine_from_config(settings, prefix='sqlalchemy.')
+        config.registry.dbmaker = sessionmaker(bind=engine)
+        config.add_request_method(db, reify=True)
         config.include('.models')
         config.include('pyramid_jinja2')
         config.include('.routes')
