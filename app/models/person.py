cf127c976020b0945a8c1b953943c6d9ba16de33
Jakob Rohrhirsch <jakob@rohrhirsch-consulting.de>
2020-03-21 17:33:33 +0100

    New alembic version Some changes to person model

--- app/models/person.py
+++ app/models/person.py
@@ -6,9 +6,11 @@ from .meta import Base
 class Person(Base):
     __tablename__ = 'persons'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    name = sqlalchemy.Column(sqlalchemy.Text)
+    firstname = sqlalchemy.Column(sqlalchemy.Text)
+    lastname = sqlalchemy.Column(sqlalchemy.Text)
     email = sqlalchemy.Column(sqlalchemy.Text)
     phone_number = sqlalchemy.Column(sqlalchemy.Text)
+    case_number = sqlalchemy.Column(sqlalchemy.Text)
     covid_status = sqlalchemy.Column(sqlalchemy.Text)
 
 
