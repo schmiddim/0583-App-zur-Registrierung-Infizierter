e77bc0fd5cf22cad6b83d72be3a03e14917fe772
Jakob Rohrhirsch <jakob@rohrhirsch-consulting.de>
2020-03-22 10:27:16 +0100

    Update person covid-status persists in DB.

--- app/views/admin_edit.py
+++ app/views/admin_edit.py
@@ -1,5 +1,6 @@
 from pyramid.view import view_config
 import logging
+from .. import models
 
 log = logging.getLogger(__name__)
 
@@ -13,17 +14,17 @@ def admin_edit_case(request):
 
     if request.method == "POST" and not has_error:
         status = request.POST['status']
-
         log.log(logging.INFO, "update status to {} for user {}".format(status, db_id))
+        person = request.dbsession.query(models.Person).get(db_id)
+        person.covid_status = status
 
-        # @todo update status for user and return the user object
-        return {"id": 1,
-                "first_name": "Michael",
-                "last_name": "Schmitt",
-                "phone_number": "0171 957 97 20",
-                "case_number": "acsmoi24bv",
-                "covid_status": "infected",
-                "email": "klaus@web.de",
+        return {"id": person.id,
+                "first_name": person.firstname,
+                "last_name": person.lastname,
+                "phone_number": person.phone_number,
+                "case_number": person.case_number,
+                "covid_status": person.covid_status,
+                "email": person.email,
                 "uddated": True
                 }
 
